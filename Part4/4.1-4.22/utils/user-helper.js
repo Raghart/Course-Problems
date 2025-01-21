a6ee1a5c7f5a77f743ce184c23ab2098d543e068
Rafael Rojas Diaz <118632412+Raghart@users.noreply.github.com>
2025-01-21 16:25:37 -0400

    Part4 exercises 4.1 - 4.22

---
+++ Part4/4.1-4.22/utils/user-helper.js
@@ -0,0 +1,18 @@
+const User = require('../models/user-model')
+const jwt = require('jsonwebtoken')
+
+const userExtractor = async(req, res, next) => {
+    const authorization = req.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        
+        const token = authorization.substring(7);
+        const decodedToken = jwt.verify(token, process.end.SECRET);
+        
+        if (decodedToken.id) {
+            req.user = await User.findById(decodedToken.id);
+        }
+    }
+    next();
+};
+
+module.exports = userExtractor
